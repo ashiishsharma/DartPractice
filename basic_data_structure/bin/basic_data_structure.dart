92c6071954ad186040e6788595e7b6f87dc2f4c3
ashish.sharma <ashish.sharma@menlosecurity.com>
2022-05-30 00:15:08 +0530

    Adding project

---
+++ basic_data_structure/bin/basic_data_structure.dart
@@ -0,0 +1,5 @@
+import 'package:basic_data_structure/basic_data_structure.dart' as basic_data_structure;
+
+void main(List<String> arguments) {
+  print('Hello world: ${basic_data_structure.calculate()}!');
+}
---
+++ basic_data_structure/test/basic_data_structure_test.dart
@@ -0,0 +1,8 @@
+import 'package:basic_data_structure/basic_data_structure.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
